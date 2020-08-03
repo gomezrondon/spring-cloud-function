cd1fc8dffb48f012bb69b4013ab3e9a766ce8f9d
gomezrondon <gomezrondon@gmail.com>
2020-08-03 17:15:52 -0600

    Migrating to Cloud Function Supplier and JIB.

--- reactive-source/src/main/java/com/gomezrondon/reactivesource/Application.java
+++ reactive-source/src/main/java/com/gomezrondon/reactivesource/Application.java
@@ -1,13 +1,26 @@
 package com.gomezrondon.reactivesource;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	@Value("${spring.kafka.bootstrap-servers:No_Kafka_IP_found}")
+	private String kafkaIp;
+
+	@Value("${function.generator.interval:5}")
+	private Integer duration;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("++++++++++++++++++ "+ kafkaIp + " Interval: "+duration);
+	}
 }
--- reactive-source/src/main/java/com/gomezrondon/reactivesource/service/DataGenerator.java
+++ reactive-source/src/main/java/com/gomezrondon/reactivesource/service/DataGenerator.java
@@ -1,34 +1,35 @@
 package com.gomezrondon.reactivesource.service;
 
 import com.gomezrondon.reactivesource.entities.Subscriber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.cloud.stream.reactive.StreamEmitter;
+import org.springframework.context.annotation.Bean;
 import reactor.core.publisher.Flux;
-
-import javax.annotation.PostConstruct;
 import java.time.Duration;
+import java.util.function.Supplier;
 
 @EnableBinding(Source.class)
 public class DataGenerator {
 
-    private final Source mysource;
     private final SubscriberGenerator generator;
 
-    public DataGenerator(Source mysource, SubscriberGenerator generator) {
-        this.mysource = mysource;
+    @Value("${function.generator.interval:5}")
+    private Integer duration;
+
+    public DataGenerator(SubscriberGenerator generator) {
         this.generator = generator;
     }
 
-    @StreamEmitter
-    @Output(Source.OUTPUT)
-    @PostConstruct
-    Flux<Subscriber> send(){
-        return Flux.interval(Duration.ofSeconds(1))
+    @Bean
+    Supplier<Flux<Subscriber>> send(){
+        return () ->Flux.interval(Duration.ofSeconds(duration))
                 .onBackpressureDrop()
                 .map(x -> generator.generate())
                 .log();
     }
+
+
+
 
 }
